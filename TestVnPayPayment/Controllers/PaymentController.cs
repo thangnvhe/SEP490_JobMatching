29c600263d5e929fb0b6de254701dd73b5bd6faa
Thangnvhe <thangshk08@gmail.com>
2025-10-12 19:53:07 +0700

    Add VnPay Payment Test Environment

---
+++ TestVnPayPayment/Controllers/PaymentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestVnPayPayment.Models;
+using TestVnPayPayment.Services;
+
+namespace TestVnPayPayment.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IVnPayService _vnPayService;
+        public PaymentController(IVnPayService vnPayService)
+        {
+
+            _vnPayService = vnPayService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePaymentUrlVnpay(PaymentInformationModel model)
+        {
+            var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+
+            return Redirect(url);
+        }
+        [HttpGet]
+        public IActionResult PaymentCallbackVnpay()
+        {
+            var response = _vnPayService.PaymentExecute(Request.Query);
+
+            return Json(response);
+        }
+
+    }
+}
