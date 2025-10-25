d24addde664884f96412b7434de308fdb88754fc
thaivdhe141209 <thaivdhe141209@fpt.edu.vn>
2025-10-25 17:23:44 +0700

    update side bar

--- JobMatchingSystem.FE/src/main.tsx
+++ JobMatchingSystem.FE/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import AppRouter from './app-router.tsx'
 import StoreProvider from './store/Provider.tsx'
@@ -7,7 +8,9 @@ import StoreProvider from './store/Provider.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StoreProvider>
-      <AppRouter />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </StoreProvider>
   </StrictMode>,
 )
---
+++ JobMatchingSystem.FE/src/pages/admin-site/Test/hehe.tsx
@@ -0,0 +1,7 @@
+export default function Hehe() {
+  return (
+    <div>
+      <h1>Hehe</h1>
+    </div>
+  )
+}
