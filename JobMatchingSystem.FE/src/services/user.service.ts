bb6ac4b1033f7670332110c719c890620750cf7f
thaivdhe141209 <thaivdhe141209@fpt.edu.vn>
2025-10-27 13:45:10 +0700

    â

--- JobMatchingSystem.FE/src/services/user.service.ts
+++ JobMatchingSystem.FE/src/services/user.service.ts
@@ -1,11 +1,11 @@
 import type { User } from '@/models/user';
 import { BaseServices } from './base.service';
 
-class UserService {
-  getAllUsers = () => BaseServices.getAll<User[]>(null, '/users');
-  getUserById = (id: string) => BaseServices.getById<User>(id, '/users');
-  updateUser = (id: string, data: Partial<User>) => BaseServices.update<User>(id, data, '/users');
-  deleteUser = (id: string) => BaseServices.delete(id, '/users');
-  changeUserStatus = (id: string, isActive: boolean) => BaseServices.update<User>(id, { isActive }, '/users');
-}
-export const userService = new UserService();
+export const UserServices = {
+  getAll: (params?: any) => BaseServices.getAll<User[]>('/users', params),
+  getById: (id: string) => BaseServices.getById<User>(id, '/users'),
+  create: (user: Partial<User>) => BaseServices.create<User>(user, '/users'),
+  update: (id: string, data: Partial<User>) => BaseServices.update<User>(id, data, '/users'),
+  delete: (id: string) => BaseServices.delete(id, '/users'),
+  changeStatus: (id: string, isActive: boolean) => BaseServices.update<User>(id, { isActive }, '/users')
+};
