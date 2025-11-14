c17acf791047a465fe4de29d937c773df8eeab82
Nguyen Huy <a1k28cbq@gmail.com>
2025-11-14 10:51:55 +0700

    login

---
+++ JobMatchingSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using JobMatchingSystem.API.DTOs.Response;
+using JobMatchingSystem.API.DTOs.Request;
+using JobMatchingSystem.API.DTOs;
+using JobMatchingSystem.API.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Claims;
+
+namespace JobMatchingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest model)
+        {
+            var response = await _authService.LoginAsync(model);
+            return Ok(APIResponse<LoginDTO>.Builder()
+                .WithResult(response)
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .Build());
+        }
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken()
+        {
+            var result = await _authService.RefreshTokenAsync();
+            return Ok(APIResponse<LoginDTO>.Builder()
+                .WithResult(result)
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .Build());
+        }
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            int userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
+            await _authService.Logout(userId);
+            return Ok(APIResponse<string>.Builder()
+                .WithResult("Logout Success")
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .Build());
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var result = await _authService.RegisterAsync(request);
+            return Ok(APIResponse<string>.Builder()
+                .WithResult(result)
+                .WithStatusCode(HttpStatusCode.Created)
+                .WithSuccess(true)
+                .Build());
+        }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request)
+        {
+            await _authService.ForgotPasswordAsync(request);
+            return Ok(APIResponse<string>.Builder()
+                .WithResult("Please check your Email")
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .Build());
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request)
+        {
+            await _authService.ResetPasswordAsync(request);
+            return Ok(APIResponse<string>.Builder()
+                .WithResult("Change Password Success")
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .Build());
+        }
+    }
+}
---
+++ JobMatchingSystem.API/Services/Implementations/AuthService.cs
@@ -0,0 +1,141 @@
+﻿using JobMatchingSystem.API.DTOs.Response;
+using JobMatchingSystem.API.DTOs.Request;
+using JobMatchingSystem.API.Exceptions;
+using JobMatchingSystem.API.Helpers;
+using JobMatchingSystem.API.Models;
+using JobMatchingSystem.API.Repositories.Interfaces;
+using JobMatchingSystem.API.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System.Web;
+
+namespace JobMatchingSystem.API.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthRepository _authRepository;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailService _emailService;
+        public AuthService(IAuthRepository authRepository,
+                       SignInManager<ApplicationUser> signInManager,
+                       IConfiguration configuration,
+                       IHttpContextAccessor httpContextAccessor,
+                       UserManager<ApplicationUser> userManager,
+                       IEmailService emailService)
+        {
+            _authRepository = authRepository;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+            _emailService = emailService;
+        }
+
+        public async Task ForgotPasswordAsync(ForgotPasswordRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+                throw new AppException(ErrorCode.EmailNotExist());
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var encodedToken = HttpUtility.UrlEncode(token);
+            var frontendUrl = _configuration["Frontend:ResetPasswordUrl"];
+            var resetLink = $"{frontendUrl}/{encodedToken}?email={user.Email}";
+            await _emailService.SendResetPasswordEmailAsync(user.Email, resetLink);
+        }
+        public async Task ResetPasswordAsync(ResetPasswordRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+                throw new AppException(ErrorCode.InvalidCredentials());
+
+            var result = await _userManager.ResetPasswordAsync(user, request.Token, request.NewPassword);
+            if (!result.Succeeded)
+            {
+                var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+                throw new Exception($"Không thể đặt lại mật khẩu: {errors}");
+            }
+        }
+
+        public async Task<LoginDTO> LoginAsync(LoginRequest request)
+        {
+            var user = await _authRepository.GetUserByEmailAsync(request.Email);
+            if (user == null)
+                throw new AppException(ErrorCode.InvalidCredentials());
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.PassWord, false);
+            if (!result.Succeeded)
+                throw new AppException(ErrorCode.InvalidCredentials());
+            var roles = await _authRepository.GetRolesAsync(user);
+            var accessToken = Untity.GenerateAccessToken(user, roles, _configuration);
+            var refreshToken = Untity.GenerateRefreshToken();
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            await _authRepository.UpdateUserAsync(user);
+            _httpContextAccessor.HttpContext!.Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = user.RefreshTokenExpiryTime
+            });
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.token = accessToken;
+            return loginDTO;
+        }
+
+        public async Task Logout(int userId)
+        {
+            var user = await _authRepository.GetUserById(userId);
+            user.RefreshToken = null;
+            user.RefreshTokenExpiryTime = null;
+            await _authRepository.UpdateUserAsync(user);
+
+        }
+
+        public async Task<LoginDTO> RefreshTokenAsync()
+        {
+            var refreshToken = _httpContextAccessor.HttpContext?.Request.Cookies["refreshToken"];
+
+            if (string.IsNullOrEmpty(refreshToken))
+                throw new AppException(ErrorCode.InValidToken());
+            var user = await _authRepository.GetUserByRefreshToken(refreshToken);
+            if (user == null || user.RefreshTokenExpiryTime < DateTime.UtcNow)
+            {
+                throw new AppException(ErrorCode.InValidToken());
+            }
+            var roles = await _authRepository.GetRolesAsync(user);
+
+            var newAccessToken = Untity.GenerateAccessToken(user, roles, _configuration);
+            return new LoginDTO
+            {
+                token = newAccessToken
+            };
+
+        }
+
+        public async Task<string> RegisterAsync(RegisterRequest request)
+        {
+
+            if (await _authRepository.ExistsAsync(request.Email))
+            {
+                throw new AppException(ErrorCode.EmailExist());
+            }
+            var user = new ApplicationUser
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                UserName = request.Email,
+                EmailConfirmed = true
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+            {
+                throw new AppException(ErrorCode.InvalidCreate());
+            }
+            await _userManager.AddToRoleAsync(user, Contraints.RoleCandidate);
+            return "Create Candidate Success";
+
+        }
+    }
+}
