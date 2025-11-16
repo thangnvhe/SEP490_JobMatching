31eecb63ee24b2c1b9f33878fb5a60d2df9ba910
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2025-11-16 21:28:11 +0700

    CRUD CVEducation

---
+++ JobMatchingSystem.API/Repositories/Implementations/CVEducationRepository.cs
@@ -0,0 +1,47 @@
+﻿using JobMatchingSystem.API.Data;
+using JobMatchingSystem.API.Models;
+using JobMatchingSystem.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobMatchingSystem.API.Repositories.Implementations
+{
+    public class CVEducationRepository : ICVEducationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CVEducationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CVEducation?> GetByIdAsync(int id)
+        {
+            return await _context.CVEducations.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<List<CVEducation>> GetByUserIdAsync(int userId)
+        {
+            return await _context.CVEducations
+                                 .Where(e => e.UserId == userId)
+                                 .ToListAsync();
+        }
+
+        public async Task CreateAsync(CVEducation education)
+        {
+            _context.CVEducations.Add(education);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(CVEducation education)
+        {
+            _context.CVEducations.Update(education);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(CVEducation education)
+        {
+            _context.CVEducations.Remove(education);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ JobMatchingSystem.API/Services/Interfaces/ICVEducationService.cs
@@ -0,0 +1,14 @@
+﻿using JobMatchingSystem.API.DTOs.Request;
+using JobMatchingSystem.API.Models;
+
+namespace JobMatchingSystem.API.Services.Interfaces
+{
+    public interface ICVEducationService
+    {
+        Task<CVEducation> GetByIdAsync(int id);
+        Task<List<CVEducation>> GetByCurrentUserAsync(int userId);
+        Task<CVEducation> CreateAsync(CVEducationRequest request, int userId);
+        Task<CVEducation> UpdateAsync(int id, CVEducationRequest request, int userId);
+        Task DeleteAsync(int id, int userId);
+    }
+}
