d3fa7179a30f6b49016ad23e31980398a3e4d89e
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2025-11-21 17:33:28 +0700

    Job taxonomy

---
+++ JobMatchingSystem.API/DTOs/Request/CreateJobTaxonomyRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace JobMatchingSystem.API.DTOs.Request
+{
+    public class CreateJobTaxonomyRequest
+    {
+        public int JobId { get; set; }
+        public int TaxonomyId { get; set; }
+    }
+}
---
+++ JobMatchingSystem.API/Repositories/Interfaces/IJobTaxonomyRepository.cs
@@ -0,0 +1,14 @@
+﻿using JobMatchingSystem.API.Models;
+
+namespace JobMatchingSystem.API.Repositories.Interfaces
+{
+    public interface IJobTaxonomyRepository
+    {
+        Task<JobTaxonomy?> GetByIdAsync(int id);
+        Task<List<JobTaxonomy>> GetByJobIdAsync(int jobId);
+        Task CreateAsync(JobTaxonomy entity);
+        Task DeleteAsync(JobTaxonomy entity);
+        Task<bool> TaxonomyExistsAsync(int taxonomyId);
+        Task<Job?> GetJobAsync(int jobId);
+    }
+}
