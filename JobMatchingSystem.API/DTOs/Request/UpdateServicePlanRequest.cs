ed54b726c332e7ba547e4f2032ab51cb34c4b6ac
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2025-11-25 15:23:08 +0700

    get - update ServicePlan

---
+++ JobMatchingSystem.API/DTOs/Request/UpdateServicePlanRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace JobMatchingSystem.API.DTOs.Request
+{
+    public class UpdateServicePlanRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
