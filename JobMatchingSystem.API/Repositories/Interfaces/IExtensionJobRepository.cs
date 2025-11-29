ec9180d3a3607280b38350c654b5fd76be5d3eac
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2025-11-30 02:23:21 +0700

    get HighlightJob - ExtensionJob

---
+++ JobMatchingSystem.API/Repositories/Interfaces/IExtensionJobRepository.cs
@@ -0,0 +1,9 @@
+﻿using JobMatchingSystem.API.Models;
+
+namespace JobMatchingSystem.API.Repositories.Interfaces
+{
+    public interface IExtensionJobRepository
+    {
+        Task<List<ExtensionJob>> GetByRecuiterIdAsync(int recuiterId);
+    }
+}
---
+++ JobMatchingSystem.API/Services/Implementations/HighlightJobService.cs
@@ -0,0 +1,21 @@
+﻿using JobMatchingSystem.API.Models;
+using JobMatchingSystem.API.Repositories.Interfaces;
+using JobMatchingSystem.API.Services.Interfaces;
+
+namespace JobMatchingSystem.API.Services.Implementations
+{
+    public class HighlightJobService : IHighlightJobService
+    {
+        private readonly IHighlightJobRepository _highlightJobRepository;
+
+        public HighlightJobService(IHighlightJobRepository highlightJobRepository)
+        {
+            _highlightJobRepository = highlightJobRepository;
+        }
+
+        public async Task<List<HighlightJob>> GetHighlightJobsByUserAsync(int userId)
+        {
+            return await _highlightJobRepository.GetByRecuiterIdAsync(userId);
+        }
+    }
+}
