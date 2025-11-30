9e05da54f7aa472848fa7367075d6a98e0305bfe
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2025-11-30 18:36:44 +0700

    ServicePlan

--- JobMatchingSystem.API/Services/Implementations/ServicePlanService.cs
+++ JobMatchingSystem.API/Services/Implementations/ServicePlanService.cs
@@ -2,6 +2,7 @@
 using JobMatchingSystem.API.DTOs.Response;
 using JobMatchingSystem.API.Exceptions;
 using JobMatchingSystem.API.Helpers;
+using JobMatchingSystem.API.Models;
 using JobMatchingSystem.API.Repositories.Interfaces;
 using JobMatchingSystem.API.Services.Interfaces;
 
@@ -24,7 +25,13 @@ namespace JobMatchingSystem.API.Services.Implementations
                 Id = x.Id,
                 Name = x.Name,
                 Description = x.Description,
-                Price = x.Price
+                Price = x.Price,
+                JobPostAdditional = x.JobPostAdditional,
+                HighlightJobDays = x.HighlightJobDays,
+                HighlightJobDaysCount = x.HighlightJobDaysCount,
+                ExtensionJobDays = x.ExtensionJobDays,
+                ExtensionJobDaysCount = x.ExtensionJobDaysCount,
+                CVSaveAdditional = x.CVSaveAdditional
             }).ToList();
         }
 
@@ -40,8 +47,32 @@ namespace JobMatchingSystem.API.Services.Implementations
                 Id = plan.Id,
                 Name = plan.Name,
                 Description = plan.Description,
-                Price = plan.Price
+                Price = plan.Price,
+                JobPostAdditional = plan.JobPostAdditional,
+                HighlightJobDays = plan.HighlightJobDays,
+                HighlightJobDaysCount = plan.HighlightJobDaysCount,
+                ExtensionJobDays = plan.ExtensionJobDays,
+                ExtensionJobDaysCount = plan.ExtensionJobDaysCount,
+                CVSaveAdditional = plan.CVSaveAdditional
             };
+        }
+
+        public async Task CreateAsync(CreateServicePlanRequest request)
+        {
+            var newPlan = new ServicePlan
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Price = request.Price,
+                JobPostAdditional = request.JobPostAdditional,
+                HighlightJobDays = request.HighlightJobDays,
+                HighlightJobDaysCount = request.HighlightJobDaysCount,
+                ExtensionJobDays = request.ExtensionJobDays,
+                ExtensionJobDaysCount = request.ExtensionJobDaysCount,
+                CVSaveAdditional = request.CVSaveAdditional
+            };
+
+            await _servicePlanRepository.AddAsync(newPlan);
         }
 
         public async Task UpdateAsync(int id, UpdateServicePlanRequest request)
@@ -59,7 +90,34 @@ namespace JobMatchingSystem.API.Services.Implementations
             if (request.Price.HasValue && request.Price.Value > 0)
                 plan.Price = request.Price.Value;
 
+            if (request.JobPostAdditional.HasValue)
+                plan.JobPostAdditional = request.JobPostAdditional;
+
+            if (request.HighlightJobDays.HasValue)
+                plan.HighlightJobDays = request.HighlightJobDays;
+
+            if (request.HighlightJobDaysCount.HasValue)
+                plan.HighlightJobDaysCount = request.HighlightJobDaysCount;
+
+            if (request.ExtensionJobDays.HasValue)
+                plan.ExtensionJobDays = request.ExtensionJobDays;
+
+            if (request.ExtensionJobDaysCount.HasValue)
+                plan.ExtensionJobDaysCount = request.ExtensionJobDaysCount;
+
+            if (request.CVSaveAdditional.HasValue)
+                plan.CVSaveAdditional = request.CVSaveAdditional;
+
             await _servicePlanRepository.UpdateAsync(plan);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var plan = await _servicePlanRepository.GetByIdAsync(id);
+            if (plan == null)
+                throw new AppException(ErrorCode.NotFoundServicePlan());
+
+            await _servicePlanRepository.DeleteAsync(plan);
         }
     }
 }
