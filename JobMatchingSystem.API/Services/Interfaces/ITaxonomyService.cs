eaae17e3d173aea225e6934a2a9a4094174f732f
Thangnvhe <thangshk08@gmail.com>
2025-12-02 01:04:09 +0700

    b

--- JobMatchingSystem.API/Services/Interfaces/ITaxonomyService.cs
+++ JobMatchingSystem.API/Services/Interfaces/ITaxonomyService.cs
@@ -1,9 +1,14 @@
-﻿using JobMatchingSystem.API.Models;
+﻿using JobMatchingSystem.API.DTOs.Response;
 
 namespace JobMatchingSystem.API.Services.Interfaces
 {
     public interface ITaxonomyService
     {
-        Task<List<Taxonomy>> GetAllTaxonomiesAsync();
+        Task<List<TaxonomyTreeResponse>> GetAllTaxonomiesAsync();
+        Task<List<TaxonomyTreeResponse>> GetTaxonomyTreeAsync();
+        Task<List<TaxonomyFlatResponse>> GetTaxonomyFlatListAsync();
+        Task<List<TaxonomyTreeResponse>> GetChildrenByParentIdAsync(int parentId);
+        Task<TaxonomyTreeResponse?> GetTaxonomyByIdAsync(int id);
+        Task<List<TaxonomyTreeResponse>> GetRootTaxonomiesAsync();
     }
 }
