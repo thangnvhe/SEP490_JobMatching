f263cd8cc0156bdf07ae66df421ba10cee8ceed0
Thangnvhe <thangshk08@gmail.com>
2025-12-06 00:44:54 +0700

    b

--- JobMatchingSystem.API/Controllers/PositionController.cs
+++ JobMatchingSystem.API/Controllers/PositionController.cs
@@ -1,6 +1,7 @@
 ﻿using JobMatchingSystem.API.DTOs;
 using JobMatchingSystem.API.DTOs.Request;
 using JobMatchingSystem.API.DTOs.Response;
+using JobMatchingSystem.API.Helpers;
 using JobMatchingSystem.API.Models;
 using JobMatchingSystem.API.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -30,6 +31,18 @@ namespace JobMatchingSystem.API.Controllers
                 .WithStatusCode(HttpStatusCode.OK)
                 .WithSuccess(true)
                 .WithResult(positions)
+                .Build());
+        }
+
+        [HttpGet("paged")]
+        public async Task<IActionResult> GetAllPaged([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string sortBy = "", [FromQuery] bool isDescending = false, [FromQuery] string search = "")
+        {
+            var pagedResult = await _positionService.GetAllPagedAsync(page, pageSize, sortBy, isDescending, search);
+
+            return Ok(APIResponse<PagedResult<PositionResponse>>.Builder()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithSuccess(true)
+                .WithResult(pagedResult)
                 .Build());
         }
 
--- JobMatchingSystem.API/Services/Interfaces/IPositionService.cs
+++ JobMatchingSystem.API/Services/Interfaces/IPositionService.cs
@@ -1,5 +1,6 @@
 ﻿using JobMatchingSystem.API.DTOs.Response;
 using JobMatchingSystem.API.DTOs.Request;
+using JobMatchingSystem.API.Helpers;
 using JobMatchingSystem.API.Models;
 
 namespace JobMatchingSystem.API.Services.Interfaces
@@ -7,6 +8,7 @@ namespace JobMatchingSystem.API.Services.Interfaces
     public interface IPositionService
     {
         Task<IEnumerable<PositionResponse>> GetAllAsync();
+        Task<PagedResult<PositionResponse>> GetAllPagedAsync(int page = 1, int pageSize = 10, string sortBy = "", bool isDescending = false, string search = "");
         Task<PositionResponse> GetByIdAsync(int id);
         Task<PositionResponse> CreateAsync(int candidateId, int positionId);
         Task UpdateCandidatePositionAsync(int candidateId, int positionId);
