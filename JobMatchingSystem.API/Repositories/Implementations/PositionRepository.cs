b86059c0a989a3c505af8ec6637a34c8b4737566
Thangnvhe <thangshk08@gmail.com>
2025-12-05 22:42:14 +0700

    b

--- JobMatchingSystem.API/Repositories/Implementations/PositionRepository.cs
+++ JobMatchingSystem.API/Repositories/Implementations/PositionRepository.cs
@@ -31,5 +31,44 @@ namespace JobMatchingSystem.API.Repositories.Implementations
             await _context.SaveChangesAsync();
             return position;
         }
+
+        public async Task<Position> UpdateAsync(Position position)
+        {
+            _context.Positions.Update(position);
+            await _context.SaveChangesAsync();
+            return position;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            // Set PositionId to null in related Jobs
+            var relatedJobs = await _context.Jobs
+                .Where(j => j.PositionId == id)
+                .ToListAsync();
+            
+            foreach (var job in relatedJobs)
+            {
+                job.PositionId = null;
+            }
+
+            // Set PositionId to null in related CVProfiles
+            var relatedCVProfiles = await _context.CVProfiles
+                .Where(cv => cv.PositionId == id)
+                .ToListAsync();
+            
+            foreach (var cvProfile in relatedCVProfiles)
+            {
+                cvProfile.PositionId = null;
+            }
+
+            // Delete the position
+            var position = await _context.Positions.FindAsync(id);
+            if (position != null)
+            {
+                _context.Positions.Remove(position);
+            }
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
