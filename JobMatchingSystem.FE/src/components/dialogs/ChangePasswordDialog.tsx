e604d5cddbe984bdf25c2acd2adc403dd3181101
thaivdhe141209 <thaivdhe141209@fpt.edu.vn>
2025-12-31 16:53:23 +0700

    a
    a

--- JobMatchingSystem.FE/src/components/dialogs/ChangePasswordDialog.tsx
+++ JobMatchingSystem.FE/src/components/dialogs/ChangePasswordDialog.tsx
@@ -114,7 +114,7 @@ export const ChangePasswordDialog: React.FC<ChangePasswordDialogProps> = ({
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={(open) => !loading && handleClose()}>
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
